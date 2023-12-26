f7fa20d9e3fc8ef371339ad3ae6063e251570b62
David Mendoza <davidmendozalainez99@gmail.com>
2023-12-26 09:42:22 -0600

    Update Harpoon keybindings

--- lua/custom/plugins/harpoon2.lua
+++ lua/custom/plugins/harpoon2.lua
@@ -6,7 +6,7 @@ return {
         require("harpoon"):setup()
     end,
     keys = {{
-        "<leader>hf",
+        "<leader>ha",
         function()
             require("harpoon"):list():append()
         end,
@@ -17,27 +17,27 @@ return {
             local harpoon = require("harpoon")
             harpoon.ui:toggle_quick_menu(harpoon:list())
         end,
-        desc = "harpoon quick menu"
+        desc = "[H]arpoon quick [m]enu"
     }, {
-        "<leader>j",
+        "<leader>1",
         function()
             require("harpoon"):list():select(1)
         end,
         desc = "harpoon to file 1"
     }, {
-        "<leader>k",
+        "<leader>2",
         function()
             require("harpoon"):list():select(2)
         end,
         desc = "harpoon to file 2"
     }, {
-        "<leader>l",
+        "<leader>3",
         function()
             require("harpoon"):list():select(3)
         end,
         desc = "harpoon to file 3"
     }, {
-        "<leader>;",
+        "<leader>4",
         function()
             require("harpoon"):list():select(4)
         end,
