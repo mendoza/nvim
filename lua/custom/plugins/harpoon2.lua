d7fde68a5707fd1614ad8bc33a05c052f05ecce0
David <davidmendozalainez99@gmail.com>
2024-04-27 18:03:36 -0600

    append is being deprecated

--- lua/custom/plugins/harpoon2.lua
+++ lua/custom/plugins/harpoon2.lua
@@ -6,7 +6,7 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup {}
     vim.keymap.set('n', '<leader>ha', function()
-      harpoon:list():append()
+      harpoon:list():add()
     end)
     vim.keymap.set('n', '<leader>hm', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
