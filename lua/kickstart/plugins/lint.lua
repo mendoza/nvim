b9760c27ba9a3663013f6385d53a66bc4d2eac8c
David <davidmendozalainez99@gmail.com>
2024-12-01 21:28:53 -0600

    linting and updating packages

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,12 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        javascript = { 'eslint' },
+        javascriptreact = { 'eslint' },
+        typescript = { 'eslint' },
+        typescriptreact = { 'eslint' },
+        cpp = nil,
+        c = nil,
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
