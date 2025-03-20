be6340acf8db0746909da6abb4a5948cd0dcbf1f
David Mendoza <davidmendozalainez99@gmail.com>
2025-03-19 20:13:37 -0600

    add priority on the formatters based on their config files

--- init.lua
+++ init.lua
@@ -992,6 +992,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  -- require 'kickstart.plugins.auto_formatting', -- add priority to formatters with present configuration files
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
