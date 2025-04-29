a1f420e19436cfd200e85f4e941e6132430ad688
David Mendoza <davidmendozalainez99@gmail.com>
2025-04-28 19:42:36 -0600

    of course they had a built in version

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,70 +16,17 @@ return {
   {
     'stevearc/conform.nvim',
     opts = function(_, opts)
-      local formatter_configs = {
-        prettier = { config_files = { '.prettierrc', '.prettierrc.json', '.prettierrc.yml', '.prettierrc.yaml', '.prettierrc.js', 'prettier.config.js' } },
-        biome = { config_files = { 'biome.json', 'biome.jsonc' } },
-        stylua = { config_files = { 'stylua.toml', '.stylua.toml' } },
+      opts.formatters = {
+        biome = {
+          require_cwd = true,
+        },
+        prettier = {
+          require_cwd = true,
+        },
+        stylua = {
+          require_cwd = true,
+        },
       }
-
-      local function find_project_root(filepath)
-        return vim.fs.root(filepath, { '.git', 'package.json' }) or vim.fn.getcwd()
-      end
-
-      local function has_config_file(formatter, filepath)
-        local config = formatter_configs[formatter]
-        if not config then
-          return false
-        end
-
-        local root = find_project_root(filepath)
-        for _, file in ipairs(config.config_files) do
-          if vim.fn.filereadable(root .. '/' .. file) == 1 then
-            return true
-          end
-        end
-        return false
-      end
-
-      local function get_formatters(formatters_to_check, fallback_order, buf)
-        local filepath = vim.api.nvim_buf_get_name(buf)
-        if filepath == '' then
-          return fallback_order or formatters_to_check
-        end
-
-        for _, formatter in ipairs(formatters_to_check) do
-          if has_config_file(formatter, filepath) then
-            return { formatter }
-          end
-        end
-        return fallback_order or formatters_to_check
-      end
-
-      local formatters = {
-        lua = function(buf)
-          return get_formatters({ 'stylua' }, { 'stylua' }, buf)
-        end,
-        javascript = function(buf)
-          return get_formatters({ 'prettier', 'biome' }, { 'prettier', 'biome' }, buf)
-        end,
-        typescript = function(buf)
-          return get_formatters({ 'prettier', 'biome' }, { 'prettier', 'biome' }, buf)
-        end,
-        typescriptreact = function(buf)
-          return get_formatters({ 'prettier', 'biome' }, { 'prettier', 'biome' }, buf)
-        end,
-        javascriptreact = function(buf)
-          return get_formatters({ 'prettier', 'biome' }, { 'prettier', 'biome' }, buf)
-        end,
-        svelte = function(buf)
-          return get_formatters({ 'prettier', 'biome' }, { 'prettier', 'biome' }, buf)
-        end,
-      }
-
-      for ft, formatter in pairs(formatters) do
-        opts.formatters_by_ft[ft] = formatter
-      end
-
       return opts
     end,
   },
