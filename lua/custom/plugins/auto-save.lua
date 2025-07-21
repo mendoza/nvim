92e7b05da1659f521701af9a36c66c99ecd56c12
David Mendoza <davidmendozalainez99@gmail.com>
2025-07-21 17:06:09 -0600

    Updated lazy-lock.json and custom plugins for dependency updates and auto-save configuration tweaks.
    Signed-off-by: David Mendoza <davidmendozalainez99@gmail.com>

--- lua/custom/plugins/auto-save.lua
+++ lua/custom/plugins/auto-save.lua
@@ -4,7 +4,7 @@ return {
     local autosave = require 'auto-save'
     autosave.setup {
       -- info: testing 500 ms delay for now
-      debounce_delay = 2000, -- delay after which a pending save is executed
+
       condition = function(buf)
         -- info: hot fix to not save and close harpoon's quick menu
         if vim.bo[buf].filetype == 'harpoon' then
